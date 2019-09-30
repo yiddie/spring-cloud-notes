bda951eca060836e327f3db4e8ce27495d5d631f
lipengguang <lpg_1994@163.com>
2019-09-30 11:21:49 +0800

    eureka启动类

---
+++ spring-cloud-notes-eureka/src/main/java/com/yiddie/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.yiddie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka启动类
+ * 使用@EnableEurekaServer注解启动EurekaServer
+ *
+ * @author yiddie
+ * @date 2019/9/30 11:13 上午
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
